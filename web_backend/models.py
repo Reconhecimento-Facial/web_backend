ef67d93fe939a8797989c50bf28674631c666808
Vinicius Cabral <viniciuscabral26@hotmail.com>
2024-10-29 15:18:40 -0300

    Configurando ambiente de desenvolvimento

---
+++ web_backend/models.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+from enum import Enum
+
+from sqlalchemy.orm import Mapped, registry
+
+table_registry = registry()
+
+
+class UserStatus(str, Enum):
+    active = 'ativo'
+    deactivated = 'desativado'
+
+
+@table_registry.mapped_as_dataclass
+class User:
+    __tablename__ = 'users'
+
+    id: Mapped[int]
+    name: Mapped[str]
+    email: Mapped[str]
+    password: Mapped[str]
+    created_at: Mapped[datetime]
+
+
+# class TodoState(str, Enum):
+#     draft = 'draft'
+#     todo = 'todo'
+#     doing = 'doing'
+#     done = 'done'
+#     trash = 'trash'
+
+
+# @table_registry.mapped_as_dataclass
+# class User:
+#     __tablename__ = 'users'
+
+#     id: Mapped[int] = mapped_column(init=False, primary_key=True)
+#     username: Mapped[str] = mapped_column(unique=True)
+#     password: Mapped[str]
+#     email: Mapped[str] = mapped_column(unique=True)
+#     created_at: Mapped[datetime] = mapped_column(
+#         init=False, server_default=func.now()
+#     )
+#     updated_at: Mapped[datetime] = mapped_column(
+#         init=False, server_default=func.now(), onupdate=func.now()
+#     )
+#     todos: Mapped[list['Todo']] = relationship(
+#         init=False, back_populates='user', cascade='all, delete-orphan'
+#     )
+
+
+# @table_registry.mapped_as_dataclass
+# class Todo:
+#     __tablename__ = 'todos'
+
+#     id: Mapped[int] = mapped_column(init=False, primary_key=True)
+#     title: Mapped[str]
+#     description: Mapped[str]
+#     state: Mapped[TodoState]
+#     created_at: Mapped[datetime] = mapped_column(
+#         init=False, server_default=func.now()
+#     )
+#     updated_at: Mapped[datetime] = mapped_column(
+#         init=False, server_default=func.now(), onupdate=func.now()
+#     )
+#     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+#     user: Mapped[User] = relationship(init=False, back_populates='todos')
