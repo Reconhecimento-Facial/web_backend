a437b726de90382b51fa2a7fed055d421d57b960
Vinicius Cabral <viniciuscabral26@hotmail.com>
2024-11-13 00:05:08 -0300

    testando banco modelos criados

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,11 +1,16 @@
 from sqlalchemy import select
 
-from web_backend.models import User, Userstatus
+from web_backend.models import (
+    Enviroment,
+    Group,
+    User,
+    UserStatus,
+)
 
 
 def test_create_user(session):
     new_user = User(
-        status=Userstatus.active,
+        status=UserStatus.disabled,
         username='alice',
         password='secret',
         email='teste@test',
@@ -16,3 +21,26 @@ def test_create_user(session):
     user = session.scalar(select(User).where(User.username == 'alice'))
 
     assert user.username == 'alice'
+    assert user.status == UserStatus.disabled
+    assert user.email == 'teste@test'
+    assert user.password == 'secret'
+
+
+def test_create_group(session):
+    new_group = Group(name='Professores')
+    session.add(new_group)
+    session.commit()
+
+    group = session.scalar(select(Group).where(Group.name == 'Professores'))
+
+    assert group.name == 'Professores'
+
+
+def test_create_env(session):
+    new_env = Enviroment(name='lab_3')
+    session.add(new_env)
+    session.commit()
+
+    env = session.scalar(select(Enviroment).where(Enviroment.name == 'lab_3'))
+
+    assert env.name == 'lab_3'
