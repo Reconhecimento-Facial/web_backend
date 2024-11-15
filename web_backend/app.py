1dd7b2d59d68375b2267097f82a70ddfea04e481
Vinicius Cabral <viniciuscabral26@hotmail.com>
2024-11-15 03:31:09 -0300

    feat: add user schemas and authentication dependencies

--- web_backend/app.py
+++ web_backend/app.py
@@ -2,11 +2,12 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from web_backend.routers import users
+from web_backend.routers import auth, users
 from web_backend.schemas import MessageSchema
 
 app = FastAPI()
 app.include_router(users.router)
+app.include_router(auth.router)
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=MessageSchema)
