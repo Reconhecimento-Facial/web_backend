968d5f23239585f647eb25e4e433b8e153792ea7
Vinicius Cabral <viniciuscabral26@hotmail.com>
2024-11-20 11:10:04 -0300

    .

--- web_backend/models.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from enum import Enum
-
-from sqlalchemy import Column, ForeignKey, Table, func
-from sqlalchemy.orm import Mapped, mapped_column, registry, relationship
-
-table_registry = registry()
