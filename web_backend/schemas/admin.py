3efc4312eb46ee0e528745a9f5d20ceeed083135
Vinicius Cabral <viniciuscabral26@hotmail.com>
2024-11-29 02:04:00 -0300

    mudando o campo de username para email

--- web_backend/schemas/admin.py
+++ web_backend/schemas/admin.py
@@ -1,6 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+
 
 class AdminSchema(BaseModel):
-    username: str
+    email: EmailStr
     password: str
     super_admin: bool
+
+
+class AdminPublicSchema(AdminSchema):
+    id: int
+
+    class Config:
+        orm_mode=True
+
