4e658b74e9e789d196aca1c6adcffc36e1a86214
Vinicius Cabral <viniciuscabral26@hotmail.com>
2025-01-10 23:30:58 -0300

    Modifying photo_url utility to return the file path

--- web_backend/utils/file_path.py
+++ web_backend/utils/file_path.py
@@ -2,13 +2,13 @@ import glob
 from pathlib import Path
 
 
-def photo_url(id: int, dir_name: str) -> str:
+def file_path(id: int, dir_name: str) -> str:
     photo_path = Path.cwd() / 'uploads' / dir_name
     photo_path = f'{photo_path}/{id}.*'
 
     files = glob.glob(photo_path)
-    file_url = ''
+    file_name = ''
     if files:
-        file_url = '/' + dir_name + '/' + Path(files[0]).name
+        file_name = Path(files[0]).name
 
-    return file_url
+    return dir_name + '/' + file_name
