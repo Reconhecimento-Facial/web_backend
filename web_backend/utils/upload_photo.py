4778cd74b99179e4825e4bf9d3c36d38c7d5c7da
Vinicius Cabral <viniciuscabral26@hotmail.com>
2025-01-15 06:41:42 -0300

    Changing upload function to overwrite photo file

--- web_backend/utils/upload_photo.py
+++ web_backend/utils/upload_photo.py
@@ -1,16 +1,23 @@
 import shutil
 from pathlib import Path
 
+import glob
+
 from fastapi import UploadFile
 
 
 def upload_photo(file: UploadFile, id: int, dir_name: str) -> None:
     upload_dir = Path.cwd() / 'uploads' / dir_name
     upload_dir.mkdir(parents=True, exist_ok=True)
+    
+    photo_path = f'{upload_dir}/{id}.*'
+    files = glob.glob(photo_path)
+    if files:
+        Path(files[0]).unlink()
 
-    extensao = Path(file.filename).suffix
-    nome_arquivo = f'{id}{extensao}'
-    file_path = upload_dir / nome_arquivo
-
-    with file_path.open('wb') as buffer:
+    extension = Path(file.filename).suffix
+    file_destiny = upload_dir / Path(f'{id}{extension}')
+    with file_destiny.open('wb') as buffer:
         shutil.copyfileobj(file.file, buffer)
+
+    return None
